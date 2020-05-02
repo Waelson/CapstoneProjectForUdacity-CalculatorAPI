41afba334dadc77f9982615fc8d6c711ab2649a7
Waelson Negreiros <waelson@gmail.com>
2020-05-02 15:13:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 
-
 APP = Flask(__name__)
 LOGGER = create_logger(app)
 LOGGER.setLevel(logging.DEBUG)
-
 
 @app.route("/")
 def home():
@@ -16,12 +14,10 @@ def home():
 def multiply():
     param1 = request.args.get(key='param1', default=0, type=int)
     if param1 == 0:
-        return jsonify({"status": "error", "message": "Invalid parameter #1", "value": param1}), 500
-    
+        return jsonify({"status": "error", "message": "Invalid parameter #1", "value": param1}), 500    
     param2 = request.args.get(key='param2', default=0, type=int)
     if param2 == 0:
         return jsonify({"status": "error", "message": "Invalid parameter #2", "value": param1}), 500
-
     result = int(param1) * int(param2)    
     return jsonify({"status": "success", "result": result}), 200
 
