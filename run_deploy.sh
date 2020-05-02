bd73f5aee7de208ce45eb0ef392cdcc2dd04ea75
Waelson Negreiros <waelson@gmail.com>
2020-05-02 14:34:20 -0300

    Update run_deploy.sh

--- run_deploy.sh
+++ run_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-krane deploy project-capstone arn:aws:eks:us-west-2:053455162894:cluster/cluster-name1 -f ./descriptors-k8s
-kubectl get services -o wide --namespace project-capstone
+krane deploy $1 $(kubectl config current-context) -f ./descriptors-k8s
+kubectl get services -o wide --namespace $1
