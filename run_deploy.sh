1cd8cfd98693e8df2ed6fa0355b918c96124064a
Waelson Negreiros <waelson@gmail.com>
2020-05-03 15:45:22 -0300

    Update run_deploy.sh

--- run_deploy.sh
+++ run_deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 krane deploy $1 $(kubectl config current-context) -f ./descriptors-k8s
 kubectl get services -o wide --namespace $1
+kubectl -n $1 set image deployments/flask-backend-deployment flask-app=thedevices/calculator-api:$2
